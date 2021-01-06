55c15caf0b2a22178941923343e265f43f404db6
chenji <cjyiz@aliyun.com>
2021-01-06 16:59:48 +0800

    feat: delete useless dist fold

--- src/components/Swiper/dist/Carousel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.App = void 0;
-var react_1 = require("react");
-exports.App = function () {
-    return (react_1["default"].createElement(react_1["default"].Fragment, null,
-        react_1["default"].createElement("div", null)));
-};
